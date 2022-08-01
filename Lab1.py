a36a52f37f031cbb896cfc96ab5157e21392f40a
laviba23-meet <LaviBanner@gmail.com>
2022-08-02 00:01:55 +0300

    Completed + Bonus

--- Lab1.py
+++ Lab1.py
@@ -1,24 +1,32 @@
-def create_youtube_video (title,discription):
-	youtube_video = {"Title": title, "Discription": discription, "Likes": 0, "dislikes":0, "Comments":{} }
-	return youtube_video
+def create_youtube_video(title,discription):
+	video={"title":title, "discription":discription ,"like":0,"dislike":0,"comments":{},"hashtag":[]}
+	for i in range(0,5):
+		video['hashtag'].append(input("Enter your value: "))
+	return video
 
-def like (youtube_video):
-	if "Likes" in youtube_video: 
-		youtube_video["Likes"]+=1
-		return youtube_video
+def like(video):
+	if "like" in video:
+		video["like"]+=1
+	return video 
 
-def dislike (youtube_video):
-	if "dislikes" in youtube_video:
-		youtube_video["dislikes"]+=1
-		return youtube_video
+def dislike(video):
+	if "dislike" in video:
+		video["dislike"]+=1
+	return video 
 
-def add_comment(youtube_video, username, comment_text):
-					youtube_video["Comments"][username] = comment_text
-					return youtube_video
-
-new_video = create_youtube_video("piano piece", "Grand Valse")
-for i in range (495):
-	new_video= like(new_video)
-	new_video = dislike(new_video)
-	new_video = add_comment(new_video, "amazing!" ,"very cool!")
-print (new_video)
+def add_comment(video,username,comment_text):
+	video["comments"][username]=comment_text
+	return video
+video=create_youtube_video(15,25)
+video1=create_youtube_video(12,25)
+like(video1)
+add_comment(video1,"omar","hello")
+print(video1)
+def similarity(video,video1):
+	count=0
+	for i in range(0,5):
+		for j in range(0,5):
+			if video1['hashtag'][i]==video1['hashtag'][j]:
+				count+=1
+	print(count*4)			
+similarity(video,video1)
